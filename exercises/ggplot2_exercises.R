b8a052bab07f01f2eee491074d5b7a46cd30c521
Anthony Chau <achau713@gmail.com>
2021-07-25 15:27:56 -0700

    Add ggplot2 exercises

---
+++ exercises/ggplot2_exercises.R
@@ -0,0 +1,59 @@
+# Exercises for ggplot2 lecture
+
+# Make sure you have installed ggplot2 before starting the exercises
+# install.packages("ggplot2")
+library(ggplot2)
+
+# Exercises
+# For non-code answers, use a comment #.
+# For the following exercises, we'll use the diamonds dataset from ggplot2
+# Try your best to improve the readability and formatting of your plot as shown in lecture.
+# Feel free to use google and the help page. 
+# Check out this link for more info: https://ggplot2.tidyverse.org/reference/diamonds.html
+diamonds <- ggplot2::diamonds
+
+# E1 ----------------------------------------------------------------------
+# Create a histogram of diamond price. Play around with the binwidth size. 
+# Describe what you see in your histogram.
+
+### YOUR CODE BELOW
+
+#################################
+
+# E2 ----------------------------------------------------------------------
+# Create a barplot of the cut of the diamond. What's the most frequent cut? Less frequent?
+
+### YOUR CODE BELOW
+
+#################################
+
+# E3 ----------------------------------------------------------------------
+# Create a scatterplot of price vs carat colored by the cut of the diamond. Describe
+# the relationship between price and carat.
+
+### YOUR CODE BELOW
+
+#################################
+
+# E4 ----------------------------------------------------------------------
+# Create a boxplot of the distribution of diamond price by the color of the diamond. Would you
+# say that the price of the diamond varies a lot depending on the color of the diamond.
+
+### YOUR CODE BELOW
+
+#################################
+
+# E5 ----------------------------------------------------------------------
+# Create a boxplot of the distribution of diamond price by the clarity of the diamond. Would you
+# say that the price of the diamond varies a lot depending on the clarity of the diamond.
+
+### YOUR CODE BELOW
+
+#################################
+
+# E6 ----------------------------------------------------------------------
+# Create any visualization that interests you
+
+### YOUR CODE BELOW
+
+#################################
